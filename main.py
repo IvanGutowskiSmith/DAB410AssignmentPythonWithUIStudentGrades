fbc80e402fba321f71b2c0adff55cab73b41b696
Ivan Gutowski-Smith <81445359+IvanGutowskiSmith@users.noreply.github.com>
2024-12-15 15:54:31 +0000

    Completed loop to print total grade pass fail count

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+#import datetime # Used for testing remove later when not needed
+#print(datetime.datetime.now())
 
 dataFrame = pd.read_csv('student_grades.csv')
 pd.set_option('display.width', 400)
@@ -6,28 +8,31 @@ pd.set_option('display.max_columns', None)
 
 # Global Variables
 TOTAL_STUDENT_COUNT = len(dataFrame) # Populate total student count on application start
-GRADE_PASS_BOUNDARY = 40 # => 40 is a pass
-
+GRADE_PASS_BOUNDARY = 40 # >= 40 is a pass
 
 print("Total Student count: "+ str(TOTAL_STUDENT_COUNT))
 
 averageAttendance = dataFrame["attendance"].mean() # Using Panda's built in mean average to calculate result, restricted to desired column for attendance
 print("Average grade is:",str(round(dataFrame["grade"].mean(),2)),"%") # Panda to average column 'grade', round to two significant figures, convert, concatenate within a string to print
 
-
 # Count pass / fails
-
 runningTotalPassCount = int() # Declare empty variable, 'none' would cause error, considered using zero however, may hide errors as program would run
 runningTotalFailCount = int()
 
-for row in dataFrame:
-    if row.grade => GRADE_PASS_BOUNDARY:
-        runningTotalPassCount ++1
-    else
-        runningTotalFailCount + +1
 
-print("Total pass = ",str(runningTotalPassCount))
-print("Total fail = ",str(runningTotalFailCount))
+
+for index,row in dataFrame.iterrows():
+    rowGrade = row['grade'] # Could combine this into if statement, however easier to debug if value is extracted to variable before being compared
+    if rowGrade >= GRADE_PASS_BOUNDARY:
+        runningTotalPassCount = runningTotalPassCount + 1 # Python does not have ++ operator to increment
+    else:
+        runningTotalFailCount = runningTotalFailCount + 1
+
+
+print("Total Pass: ", str(runningTotalPassCount))
+print("Total Fail: ",str(runningTotalFailCount))
+
+
 
 
 
