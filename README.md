78544ddc92bc8acabf0079c0e06c02bffeb43682
Ivan Gutowski-Smith <81445359+IvanGutowskiSmith@users.noreply.github.com>
2024-12-18 23:04:54 +0000

    committing pack layout before refactoring to grid
    Tried using pack layout, not particularly logical so committing that work, and trying grid layout

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 -- Required modules: / Prerequisites
 
--- Pandas python package via pycharm, selelect latest version
+-- Latest python interpreter from Python.org or Windows store (Install this first as packages seem to need re-installing after version update)
+-- Pandas python package via pycharm, select latest version
 -- numpy
 -- import tkinter, however no install required as it's part of core Python stack
 -- need git
--- main.py
+++ main.py
@@ -1,8 +1,56 @@
+from re import search
+
 import pandas as pd
 import numpy as np # np is alias, not needed but this is best practice
+
 import tkinter as tk
-import datetime # Used for testing remove later when not needed
+from tkinter import ttk
+#import datetime # Used for testing remove later when not needed
 #print(datetime.datetime.now())
+
+
+# Setup UI
+window = tk.Tk()
+window.title('Student Grade Manager')
+window.geometry('1280x720') # 16:9 aspect ratio that should fit on most screens
+
+appFont = 'Calibri'
+
+# Main packs
+left_filter_pane = ttk.Frame(master = window)
+center_results_pane = ttk.Frame(master = window)
+right_student_info_pane = ttk.Frame(master = window)
+
+# Main pack layouts
+left_filter_pane.pack(side = 'left')
+
+# Title text
+stats_Summary = ttk.Label(master = window, text = 'Student Grade Summaries', font = appFont,borderwidth=2, relief='groove') # Label widget, can add size and bold to appFont
+stats_Summary.pack()
+
+# Student search
+search_frame = ttk.Frame(master = left_filter_pane) # Creating frame for search box widgets
+search_box = ttk.Entry(master = search_frame) # lives inside the search frame, 'Entry' used as it's single line and not multiline input
+search_button = ttk.Button(master = search_frame, text = 'Search')
+# Student search layout
+
+search_frame.pack(side = 'top') # Higher level pack that contains below search box and search button
+search_box.pack(side = 'left', padx = 10) # Side argument places both on same line
+search_button.pack(side = 'left', padx = 5) # Pad puts gap on x axis to space between search box and button
+
+
+
+
+# Run UI
+window.mainloop()
+
+
+
+
+
+
+
+# Import file / logic
 
 dataFrame = pd.read_csv('student_grades.csv')
 pd.set_option('display.width', 400)
